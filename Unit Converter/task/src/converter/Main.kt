d89a219762bb51729042504cc6e416f1c6d04897
KonstBeliakov <konstbeliakov@gmail.com>
2025-01-23 19:07:55 +0300

    Unit now is enum class

--- Unit Converter/task/src/converter/Main.kt
+++ Unit Converter/task/src/converter/Main.kt
@@ -1,44 +1,44 @@
 package converter
 
-class Unit(val name: String, val nameMany: String, val value: Double, val type: String)
+enum class Unit(val nameOne: String, val nameMany: String, val value: Double, val type: String){
+    M("meter", "meters", 1.0, "length"),
+    KM("kilometer", "kilometers", 1000.0, "length"),
+    CM("centimeter", "centimeters", 0.01, "length"),
+    MM("milimeter", "milimeters", 0.001, "length"),
+    MI("mile", "miles", 1609.35, "length"),
+    YD("yard", "yards", 0.9144, "length"),
+    FT("foot", "feet", 0.3048, "length"),
+    INCH("inch", "inches", 0.0254, "length"),
+
+    G("gram", "grams", 1.0, "mass"),
+    KG("kilogram", "kilograms", 1000.0, "mass"),
+    MG("milligram", "milligrams", 0.001, "mass"),
+    LB("pound", "pounds", 453.592, "mass"),
+    OZ("ounce", "ounces", 28.3495, "mass"),
+
+    UNDEFINED("???", "???", 0.0, "???")
+}
 
 fun format(unit:Unit, number: Double): String{
-    return "$number ${if (number == 1.0) unit.name else unit.nameMany}"
+    return "$number ${if (number == 1.0) unit.nameOne else unit.nameMany}"
 }
 
 fun main() {
-    val m = Unit("meter", "meters", 1.0, "length")
-    val km = Unit("kilometer", "kilometers", 1000.0, "length")
-    val cm = Unit("centimeter", "centimeters", 0.01, "length")
-    val mm = Unit("milimeter", "milimeters", 0.001, "length")
-    val mi = Unit("mile", "miles", 1609.35, "length")
-    val yd = Unit("yard", "yards", 0.9144, "length")
-    val ft = Unit("foot", "feet", 0.3048, "length")
-    val inch = Unit("inch", "inches", 0.0254, "length")
-
-    val g = Unit("gram", "grams", 1.0, "mass")
-    val kg = Unit("kilogram", "kilograms", 1000.0, "mass")
-    val mg = Unit("milligram", "milligrams", 0.001, "mass")
-    val lb = Unit("pound", "pounds", 453.592, "mass")
-    val oz = Unit("ounce", "ounces", 28.3495, "mass")
-
-    val undefinedUnit = Unit("???", "???", 0.0, "???")
-
     val units: Map<String, Unit> = mapOf(
-        "m" to m, "meter" to m, "meters" to m,
-        "km" to km, "kilometer" to km, "kilometers" to km,
-        "cm" to cm, "centimeter" to cm, "centimeters" to cm,
-        "mm" to mm, "millimeter" to mm, "millimeters" to mm,
-        "mi" to mi, "mile" to mi, "miles" to mi,
-        "yd" to yd, "yard" to yd, "yards" to yd,
-        "ft" to ft, "foot" to ft, "feet" to ft,
-        "in" to inch, "inch" to inch, "inches" to inch,
+        "m" to Unit.M, "meter" to Unit.M, "meters" to Unit.M,
+        "km" to Unit.KM, "kilometer" to Unit.KM, "kilometers" to Unit.KM,
+        "cm" to Unit.CM, "centimeter" to Unit.CM, "centimeters" to Unit.CM,
+        "mm" to Unit.MM, "millimeter" to Unit.MM, "millimeters" to Unit.MM,
+        "mi" to Unit.MI, "mile" to Unit.MI, "miles" to Unit.MI,
+        "yd" to Unit.YD, "yard" to Unit.FT, "yards" to Unit.FT,
+        "ft" to Unit.FT, "foot" to Unit.FT, "feet" to Unit.FT,
+        "in" to Unit.INCH, "inch" to Unit.INCH, "inches" to Unit.INCH,
 
-        "g" to g, "gram" to g, "grams" to g,
-        "kg" to kg, "kilogram" to kg, "kilograms" to kg,
-        "mg" to mg, "milligram" to mg, "millirams" to mg,
-        "lb" to lb, "pound" to lb, "pounds" to lb,
-        "oz" to oz, "ounce" to oz, "ounces" to oz
+        "g" to Unit.G, "gram" to Unit.G, "grams" to Unit.G,
+        "kg" to Unit.KG, "kilogram" to Unit.KG, "kilograms" to Unit.KG,
+        "mg" to Unit.MG, "milligram" to Unit.MG, "millirams" to Unit.MG,
+        "lb" to Unit.LB, "pound" to Unit.LB, "pounds" to Unit.LB,
+        "oz" to Unit.OZ, "ounce" to Unit.OZ, "ounces" to Unit.OZ
     )
 
     while(true) {
@@ -57,8 +57,8 @@ fun main() {
                 println("${format(units[unitFrom]!!, numberFrom)} is ${format(units[unitTo]!!, numberTo)}")
             }
         }else {
-            val unitName1 = units.getOrDefault(unitFrom, undefinedUnit).nameMany
-            val unitName2 = units.getOrDefault(unitTo, undefinedUnit).nameMany
+            val unitName1 = units.getOrDefault(unitFrom, Unit.UNDEFINED).nameMany
+            val unitName2 = units.getOrDefault(unitTo, Unit.UNDEFINED).nameMany
             println("Conversion from $unitName1 to $unitName2 is impossible")
         }
     }
